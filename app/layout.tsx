3fa90d9a249e9a922f69a7a549229d15602d07f4
carmelc <carmelc@wix.com>
2023-05-01 15:43:50 +0300

    dev - bump version and alignments

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,21 @@ export default function RootLayout({
         <link rel="icon" href="https://www.wix.com/favicon.ico" />
       </head>
       <body className="text-blue-site bg-white">
-        <Header />
-        <main className="bg-white min-h-[600px]">{children}</main>
-        <div className="mt-10 sm:mt-20">
-          <Footer />
-        </div>
+        {process.env.NEXT_PUBLIC_WIX_CLIENT_ID ? (
+          <>
+            <Header />
+            <main className="bg-white min-h-[600px]">{children}</main>
+            <div className="mt-10 sm:mt-20">
+              <Footer />
+            </div>
+          </>
+        ) : (
+          <div className="bg-white min-h-[600px] max-w-5xl mx-auto p-5">
+            Page not available. Please add an environment variable called
+            NEXT_PUBLIC_WIX_CLIENT_ID, containing the client ID, to your
+            deployment provider.
+          </div>
+        )}
       </body>
     </html>
   );
