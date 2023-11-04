f5a35f8cad61940d3d252cd838de103db275fd57
vsidera <siderravictor@gmail.com>
2023-11-04 11:01:39 +0300

    add logo

--- app/components/Layout/Header.jsx
+++ app/components/Layout/Header.jsx
@@ -1,11 +1,12 @@
 import { NavBar } from '@app/components/Layout/NavBar/NavBar';
 import { Logo } from '@app/components/Logo/Logo';
 import testIds from '@app/utils/test-ids';
+import LogoImage from '../../../public/images/placeholder.jpg'
 
 const Header = () => (
   <>
     <header
-      className="w-full my-6 px-2 sm:px-8"
+      className="w-full my-6 px-0 sm:px-8"
       data-testid={testIds.LAYOUT.HEADER}
     >
       <div className="flex sm:px-6 sm:px-14 h-header sm:items-center sm:gap-4 sm:gap-8">
@@ -14,10 +15,11 @@ const Header = () => (
             href="/"
             className="flex flex-col sm:flex-row items-center gap-2 sm:gap-6"
           >
-            <Logo />
+            <img src="/images/logoimg.png" alt="Razor Talent" style={{ width: '100px', height: 'auto' }}/>
+            {/* <Logo /> */}
             <div>
-              <span className="block font-site">ChoosEquality</span>
-              <span className="text-xs">Education for All</span>
+              <span className="block font-site">Razor Talent</span>
+              <span className="text-xs" style={{ color: '#BE9704' }}>Upskill Simply</span>
             </div>
           </a>
         </h2>
